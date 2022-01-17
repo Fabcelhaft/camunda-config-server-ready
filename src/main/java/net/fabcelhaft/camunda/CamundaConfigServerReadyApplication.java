a06254e4ece2a43daac565f64ea5b31d651c8689
Kikkirej <5462557+Kikkirej@users.noreply.github.com>
2022-01-17 17:23:54 +0100

    🎉 start.spring.io generated skeleton

---
+++ src/main/java/net/fabcelhaft/camunda/CamundaConfigServerReadyApplication.java
@@ -0,0 +1,13 @@
+package net.fabcelhaft.camunda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamundaConfigServerReadyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CamundaConfigServerReadyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/fabcelhaft/camunda/CamundaConfigServerReadyApplicationTests.java
@@ -0,0 +1,13 @@
+package net.fabcelhaft.camunda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamundaConfigServerReadyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
